b862210b4300405491c9c070c0f688359c6d4ac7
igoraserpac <igoraserpac@gmail.com>
2022-09-19 20:06:23 -0300

    listas

---
+++ beecrowd/condicoes/sort_simples.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main(){
+    int a, b, c, menor, meio, maior;
+    scanf("%d %d %d", &a, &b, &c);
+    if(a>=b && a>=c){
+        maior = a;
+        if(b>=c){
+            meio = b;
+            menor = c;
+        }else{
+            meio = c;
+            menor = b;
+        }
+    }else if(b>a && b>=c){
+        maior = b;
+        if(a>=c){
+            meio = a;
+            menor = c;
+        }else{
+            meio = c;
+            menor = a;
+        }
+    }else if(c>a && c>b){
+        maior = c;
+        if(b>=a){
+            meio = b;
+            menor = a;
+        }else{
+            meio = a;
+            menor = b;
+        }
+    }
+    printf("%d\n%d\n%d\n\n", menor, meio, maior);
+    printf("%d\n%d\n%d\n", a, b, c);
+    return 0;
+}
