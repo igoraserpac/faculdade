a908aff83a093ee7505c6506a68e40ae0d0d1265
igoraserpac <igoraserpac@gmail.com>
2022-10-31 21:51:53 -0300

    procurando subsequencias

--- beecrowd/vetores/procurando_subsequencias.c
+++ beecrowd/vetores/procurando_subsequencias.c
@@ -1,31 +1,36 @@
 #include <stdio.h>
+#include <string.h>
 
-void search(char n1[11], char n2[33]){
-    int i = 0, j = 0;
-    int tem_ssq = 0;
-    while(n2[j] != '\0'){
-        while(1){
-            if(n1[i] == '\0'){
-                tem_ssq = 1;
-            }
-            if(n1[i] == n2[j]){
-                printf("n1 i %c %d \n", n1[i], i);
-                printf("n2 j %c %d \n", n2[j], j);
-                j++;
-                i++;
-            }else{
-                i = 0;
-                continue;
-            }
-        }
-        j++;
+void search(char *comparacao, char *fonte){
+    int cont = 0;
+    char *save;
+    char *ocorrencia = strstr(fonte, comparacao);
+    while(ocorrencia != NULL){
+        cont++;
+        save = ocorrencia;
+        ocorrencia += strlen(comparacao);
+        ocorrencia = strstr(ocorrencia, comparacao);
     }
+    if(cont == 0){
+        printf("Nao existe subsequencia\n");
+    }else{
+        printf("Qtd.Subsequencias: %d\n", cont);
+        printf("Pos: %ld\n", save - fonte + 1);
+    }
+    printf("\n");
 }
 
 int main(){
-    char a[11], b[33];
-    scanf("%s", a);
-    scanf("%s", b);
-    search(a, b);
+    char comparacao[11], fonte[33];
+    int caso = 1;
+    scanf("%s", comparacao);
+    scanf("%s", fonte);
+    while(!feof(stdin)){
+        printf("Caso #%d:\n", caso);
+        search(comparacao, fonte);
+        scanf("%s", comparacao);
+        scanf("%s", fonte);
+        caso++;
+    }
     return 0;
 }
--- teste.c
+++ teste.c
@@ -1,18 +1,8 @@
 #include <stdio.h>
 
-int contador(long int n, int k, int cont){
-    if(n == 0){
-        return ++cont;
-    }else if(n < k){
-        return cont;
-    }else if(n % 10 == k){
-        return contador(n/10, k, ++cont);
-    }else{
-        return contador(n/10, k, cont);
-    }
-}
-
 int main(){
-    printf("%d\n", contador(7620200922, 2, 0));
-    return 0;
+    long long int a;
+    long int b;
+    int c;
+    printf("%lu %lu %lu", sizeof(a), sizeof(b), sizeof(c));
 }
